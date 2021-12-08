6efa04390c2aef5489faaf20f479793124d8e52b
Sergio Villarroel Fernandez <svillarr@ucm.es>
2021-12-08 14:03:34 +0100

    cambios estilo

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -13,6 +13,7 @@ export default function HomeScreen() {
   return (
     <div className="home">
 
+      <div className="home1">
       <div className="left">
         <p className="title">Explota tu pasión</p>
         <p className="subtitle"> El diseño de esta prenda ha sido creado en especial para grafiteros y skaters. (Poner un pequeño texto, describiendo algo más de la ética de la marca) </p>
@@ -20,7 +21,32 @@ export default function HomeScreen() {
       <div className="rigth">
 
       </div>
-    
+        
+      </div>
+      <div className="home3">
+
     </div>
+      <div className="home2">
+
+      </div>
+      <div className="home3">
+
+    </div>
+     
+    <div className="home4">
+      <div className="divLeft"></div>
+      <div className="home4Img"></div>    
+      <div className= "divRigth"></div>
+      </div>
+      <div className="home3"></div >
+    
+    <div className="home5">
+      <div className="home5left"></div>
+      <div className="home5rigth"></div>
+    </div>
+    </div>
+ 
+ 
+    
   );
 }
