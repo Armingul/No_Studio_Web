de27e9ebe240f741fcc4afbd79c6cdde62edc58a
Sergio Villarroel <svillarr@ucm.es>
2022-03-02 20:33:50 +0100

    order

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -3,31 +3,35 @@ import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import Product from '../models/productModel.js';
 
-
 const productRouter = express.Router();
 
-
-productRouter.get('/', expressAsyncHandler(async (req, res)=>{
+productRouter.get(
+  '/',
+  expressAsyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.send(products);
-})
+  })
 );
 
 productRouter.get(
   '/seed',
   expressAsyncHandler(async (req, res) => {
-    //await Product.remove({});
-    const createdProduct = await Product.insertMany(data.products);
-    res.send({ createdProduct });
+    // await Product.remove({});
+    const createdProducts = await Product.insertMany(data.products);
+    res.send({ createdProducts });
   })
 );
 
-productRouter.get('/:id', expressAsyncHandler(async(req, res)=>{
+productRouter.get(
+  '/:id',
+  expressAsyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
-    if(product){
-        res.send(product);
-    }else{
-        res.status(400).send({message: 'Product not found'});
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: 'Product Not Found' });
     }
-}))
+  })
+);
+
 export default productRouter;
